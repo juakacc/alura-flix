205986e333685af05bfd940e5c171e2b92a508b7
Joaquim <juakacc@gmail.com>
2020-07-31 00:05:08 -0300

    Finalizando 2 aula

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 import Logo from "../../assets/img/Logo.png";
 
@@ -10,11 +11,11 @@ import "./Menu.css";
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img src={Logo} alt="AluraFlix" className="Logo" />
-      </a>
+      </Link>
 
-      <Button as="a" href="/" className="ButtonLink">
+      <Button as={Link} to="/cadastro/video" className="ButtonLink">
         Novo vídeo
       </Button>
     </nav>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 import "./index.css";
-import App from "./App";
+import Home from "./pages/Home";
+import CadastroVideo from "./pages/Cadastro/Video";
+import CadastroCategoria from "./pages/Cadastro/Categoria";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route component={Home} path="/" exact />
+      <Route component={CadastroVideo} path="/cadastro/video" />
+      <Route component={CadastroCategoria} path="/cadastro/categoria" />
+      <Route component={() => <div>Página 404</div>} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
